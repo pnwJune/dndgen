6bc36e88796227b562a7af5f454d51e726d84de3
June Ma <june.liang.ma@gmail.com>
2018-09-23 11:42:18 -0700

    new solution for console app

---
+++ dndgen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dndgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
